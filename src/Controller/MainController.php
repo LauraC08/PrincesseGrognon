d68461564028a38870428f0f9aaeed2443e63886
LauraC08 <laura-chenais@hotmail.fr>
2021-09-24 11:45:50 +0200

    Création des dernières pages + routing navbar

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -18,12 +18,42 @@ class MainController extends AbstractController
         ]);
     }
 
-     /**
-     * @Route("/bd", name="main_bd")
-     */
+    /**
+    * @Route("/bd", name="main_bd")
+    */
     public function bd(): Response
     {
         return $this->render('main/bd.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+    * @Route("/goodies", name="main_goodies")
+    */
+    public function goodies(): Response
+    {
+        return $this->render('main/goodies.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+    * @Route("/artiste", name="main_artiste")
+    */
+    public function artiste(): Response
+    {
+        return $this->render('main/artiste.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+    * @Route("/contact", name="main_contact")
+    */
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', [
             'controller_name' => 'MainController',
         ]);
     }
